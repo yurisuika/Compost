74781b5a3c4228344f3a34df487567511c8c7ee7
yurisuika <yurisuika@protonmail.com>
2022-12-08 15:32:21 -0800

    Integration for Roughly Enough Items

---
+++ src/main/java/com/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
@@ -0,0 +1,37 @@
+package com.yurisuika.compost.integration.roughlyenoughitems;
+
+import com.google.common.collect.Iterators;
+import com.yurisuika.compost.Compost;
+import com.yurisuika.compost.CompostConfig;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import me.shedaniel.rei.plugin.common.displays.DefaultCompostingDisplay;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.ComposterBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+@Environment(EnvType.CLIENT)
+public class CompostClientPlugin implements REIClientPlugin {
+
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        for (CompostConfig.Group group : Compost.config.items) {
+            Iterator<List<EntryIngredient>> iterator = Iterators.partition(ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.object2FloatEntrySet().stream().sorted(Map.Entry.comparingByValue()).map(entry -> EntryIngredients.of(entry.getKey())).iterator(), 35);
+            while (iterator.hasNext()) {
+                List<EntryIngredient> entries = iterator.next();
+                registry.add(new DefaultCompostingDisplay(entries, Collections.singletonList(EntryIngredients.of(new ItemStack(Registry.ITEM.get(new Identifier(group.item)))))));
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/yurisuika/compost/mixin/mods/RoughlyEnoughItemsMixin.java
@@ -0,0 +1,23 @@
+package com.yurisuika.compost.mixin.mods;
+
+import me.shedaniel.rei.plugin.client.DefaultClientPlugin;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+import java.util.Iterator;
+
+public class RoughlyEnoughItemsMixin {
+
+    @Mixin(DefaultClientPlugin.class)
+    public static class DefaultClientPluginMixin {
+
+        @Redirect(method = "registerDisplays", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/block/ComposterBlock;registerDefaultCompostableItems()V"), to = @At(value = "INVOKE", target = "Lme/shedaniel/rei/plugin/client/DefaultClientPlugin$DummyAxeItem;getStrippedBlocksMap()Ljava/util/Map;")))
+        private boolean redirectRegisterDisplays(Iterator instance) {
+            return false;
+        }
+
+    }
+
+}
