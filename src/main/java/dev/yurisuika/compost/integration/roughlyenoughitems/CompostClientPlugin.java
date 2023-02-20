388daf9ca57ebbe4a4be35ebcc02114737a0b8f4
yurisuika <yurisuika@protonmail.com>
2023-02-20 03:47:53 -0800

    Cut out more redundancy

--- src/main/java/dev/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
+++ src/main/java/dev/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
@@ -1,7 +1,6 @@
 package dev.yurisuika.compost.integration.roughlyenoughitems;
 
 import com.google.common.collect.Iterators;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import dev.yurisuika.compost.Compost;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
@@ -11,14 +10,10 @@ import me.shedaniel.rei.plugin.common.displays.DefaultCompostingDisplay;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.ComposterBlock;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.StringNbtReader;
-import net.minecraft.registry.Registries;
-import net.minecraft.util.Identifier;
 
 import java.util.*;
+
+import static dev.yurisuika.compost.Compost.*;
 
 @Environment(EnvType.CLIENT)
 public class CompostClientPlugin implements REIClientPlugin {
@@ -28,27 +23,7 @@ public class CompostClientPlugin implements REIClientPlugin {
         Arrays.stream(Compost.config.items).forEach(group -> {
             Iterator<List<EntryIngredient>> iterator = Iterators.partition(ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.object2FloatEntrySet().stream().sorted(Map.Entry.comparingByValue()).map(entry -> EntryIngredients.of(entry.getKey())).iterator(), 35);
             while (iterator.hasNext()) {
-                int index;
-                Item item;
-                if (group.item.contains("{")) {
-                    index = group.item.indexOf("{");
-                    item = Registries.ITEM.get(new Identifier(group.item.substring(0, index)));
-                } else {
-                    index = 0;
-                    item = Registries.ITEM.get(new Identifier(group.item));
-                }
-                ItemStack itemStack = new ItemStack(item);
-                if (group.item.contains("{")) {
-                    NbtCompound nbt;
-                    try {
-                        nbt = StringNbtReader.parse(group.item.substring(index));
-                        itemStack.setNbt(nbt);
-                    } catch (CommandSyntaxException e) {
-                        e.printStackTrace();
-                    }
-                }
-                List<EntryIngredient> entries = iterator.next();
-                registry.add(new DefaultCompostingDisplay(entries, Collections.singletonList(EntryIngredients.of(itemStack))));
+                registry.add(new DefaultCompostingDisplay(iterator.next(), Collections.singletonList(EntryIngredients.of(createItemStack(group)))));
             }
         });
     }
--- src/main/java/dev/yurisuika/compost/mixin/block/ComposterBlockMixin.java
+++ src/main/java/dev/yurisuika/compost/mixin/block/ComposterBlockMixin.java
@@ -1,7 +1,6 @@
 package dev.yurisuika.compost.mixin.block;
 
 import com.google.common.collect.Lists;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import dev.yurisuika.compost.Compost;
 import dev.yurisuika.compost.block.ArrayComposterInventory;
 import net.minecraft.block.BlockState;
@@ -9,12 +8,7 @@ import net.minecraft.block.ComposterBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.inventory.SidedInventory;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.StringNbtReader;
-import net.minecraft.registry.Registries;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
@@ -28,6 +22,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+
+import static dev.yurisuika.compost.Compost.*;
 
 @Mixin(ComposterBlock.class)
 public abstract class ComposterBlockMixin {
@@ -44,27 +40,7 @@ public abstract class ComposterBlockMixin {
                 double x = (double)(world.random.nextFloat() * 0.7F) + 0.15000000596046448D;
                 double y = (double)(world.random.nextFloat() * 0.7F) + 0.06000000238418579D + 0.6D;
                 double z = (double)(world.random.nextFloat() * 0.7F) + 0.15000000596046448D;
-                Item item;
-                int index;
-                if (group.item.contains("{")) {
-                    index = group.item.indexOf("{");
-                    String id = group.item.substring(0, index);
-                    item = Registries.ITEM.get(new Identifier(id));
-                } else {
-                    index = 0;
-                    item = Registries.ITEM.get(new Identifier(group.item));
-                }
-                ItemStack itemStack = new ItemStack(item, ThreadLocalRandom.current().nextInt(group.min, group.max + 1));
-                if (group.item.contains("{")) {
-                    NbtCompound nbt;
-                    try {
-                        nbt = StringNbtReader.parse(group.item.substring(index));
-                        itemStack.setNbt(nbt);
-                    } catch (CommandSyntaxException e) {
-                        e.printStackTrace();
-                    }
-                }
-                ItemEntity itemEntity = new ItemEntity(world, (double)pos.getX() + x, (double)pos.getY() + y, (double)pos.getZ() + z, itemStack);
+                ItemEntity itemEntity = new ItemEntity(world, (double)pos.getX() + x, (double)pos.getY() + y, (double)pos.getZ() + z, createItemStack(group));
                 itemEntity.setToDefaultPickupDelay();
                 world.spawnEntity(itemEntity);
             }
@@ -76,27 +52,7 @@ public abstract class ComposterBlockMixin {
         List<ItemStack> list = Lists.newArrayList();
         Arrays.stream(Compost.config.items).forEach(group -> {
             if(ThreadLocalRandom.current().nextDouble() < group.chance) {
-                Item item;
-                int index;
-                if (group.item.contains("{")) {
-                    index = group.item.indexOf("{");
-                    String id = group.item.substring(0, index);
-                    item = Registries.ITEM.get(new Identifier(id));
-                } else {
-                    index = 0;
-                    item = Registries.ITEM.get(new Identifier(group.item));
-                }
-                ItemStack itemStack = new ItemStack(item, ThreadLocalRandom.current().nextInt(group.min, group.max + 1));
-                if (group.item.contains("{")) {
-                    NbtCompound nbt;
-                    try {
-                        nbt = StringNbtReader.parse(group.item.substring(index));
-                        itemStack.setNbt(nbt);
-                    } catch (CommandSyntaxException e) {
-                        e.printStackTrace();
-                    }
-                }
-                list.add(itemStack);
+                list.add(createItemStack(group));
             }
         });
         if (Compost.config.shuffle) {
