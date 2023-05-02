e064d1e8bae80f7b75ad959e67c841a82cb760ac
yurisuika <yurisuika@protonmail.com>
2023-05-02 06:35:28 -0700

    23w17a

--- src/main/java/dev/yurisuika/compost/mixin/block/BlocksMixin.java
+++ src/main/java/dev/yurisuika/compost/mixin/block/BlocksMixin.java
@@ -4,7 +4,6 @@ import dev.yurisuika.compost.block.ComposterBlock;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.MapColor;
-import net.minecraft.block.Material;
 import net.minecraft.block.enums.Instrument;
 import net.minecraft.sound.BlockSoundGroup;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,7 +16,7 @@ public class BlocksMixin {
 
     @Redirect(method = "<clinit>", slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=composter")), at = @At(value = "NEW", target = "net/minecraft/block/ComposterBlock", ordinal = 0))
     private static net.minecraft.block.ComposterBlock redirectComposter(AbstractBlock.Settings settings) {
-        return new ComposterBlock(AbstractBlock.Settings.of(Material.GENERIC, MapColor.OAK_TAN).method_51368(Instrument.BASS).strength(0.6f).sounds(BlockSoundGroup.WOOD).burnable());
+        return new ComposterBlock(AbstractBlock.Settings.of().mapColor(MapColor.OAK_TAN).instrument(Instrument.BASS).strength(0.6f).sounds(BlockSoundGroup.WOOD).burnable());
     }
 
 }
