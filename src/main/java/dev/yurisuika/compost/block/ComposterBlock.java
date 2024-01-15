400a7bb3fcc984055db3826abe6bfe828608f2c1
yurisuika <yurisuika@protonmail.com>
2024-01-15 06:10:11 -0800

    drop inventory contents on break

--- src/main/java/dev/yurisuika/compost/block/ComposterBlock.java
+++ src/main/java/dev/yurisuika/compost/block/ComposterBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
@@ -15,6 +16,7 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
@@ -35,6 +37,18 @@ public class ComposterBlock extends net.minecraft.block.ComposterBlock implement
     @Override
     public BlockEntity createBlockEntity(BlockView world) {
         return new ComposterBlockEntity();
+    }
+
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if (!state.isOf(newState.getBlock())) {
+            BlockEntity blockentity = world.getBlockEntity(pos);
+            if (blockentity instanceof Inventory) {
+                ItemScatterer.spawn(world, pos, (Inventory)blockentity);
+                world.updateComparators(pos, this);
+            }
+            super.onStateReplaced(state, world, pos, newState, moved);
+        }
     }
 
     @Override
