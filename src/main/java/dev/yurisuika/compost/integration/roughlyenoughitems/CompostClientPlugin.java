d236470fa10e4d18e80fecef270e54533c962d24
yurisuika <yurisuika@protonmail.com>
2024-04-27 16:06:29 -0700

    make REI plugin cycle through item stacks

--- src/main/java/dev/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
+++ src/main/java/dev/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
@@ -1,14 +1,14 @@
 package dev.yurisuika.compost.integration.roughlyenoughitems;
 
 import com.google.common.collect.Iterators;
-import it.unimi.dsi.fastutil.objects.Object2FloatMap;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
 import me.shedaniel.rei.api.common.util.EntryIngredients;
+import me.shedaniel.rei.api.common.util.EntryStacks;
 import me.shedaniel.rei.forge.REIPluginClient;
 import me.shedaniel.rei.plugin.common.displays.DefaultCompostingDisplay;
 import net.minecraft.block.ComposterBlock;
-import net.minecraft.item.ItemConvertible;
 
 import java.util.*;
 
@@ -19,14 +19,17 @@ public class CompostClientPlugin implements REIClientPlugin {
 
     @Override
     public void registerDisplays(DisplayRegistry registry) {
+        EntryIngredient.Builder output = EntryIngredient.builder();
+        int page = 0;
+
         Arrays.stream(config.items).forEach(group -> {
-            int page = 0;
-            Iterator<List<Object2FloatMap.Entry<ItemConvertible>>> iterator = Iterators.partition(ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.object2FloatEntrySet().stream().sorted(Map.Entry.comparingByValue()).iterator(), 35);
-            while (iterator.hasNext()) {
-                List<Object2FloatMap.Entry<ItemConvertible>> entries = iterator.next();
-                registry.add(DefaultCompostingDisplay.of(entries, Collections.singletonList(EntryIngredients.of(createItemStack(group))), page++));
-            }
+            output.add(EntryStacks.of(createItemStack(group)));
         });
+
+        Iterator<List<EntryIngredient>> iterator = Iterators.partition(ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.object2FloatEntrySet().stream().sorted(Map.Entry.comparingByValue()).map(entry -> EntryIngredients.of(entry.getKey())).iterator(), 35);
+        while (iterator.hasNext()) {
+            registry.add(new DefaultCompostingDisplay(iterator.next(), List.of(output.build()), page++));
+        }
     }
 
 }
