da8d4d7e8633bd0a6799199f43381b546599f9e7
yurisuika <yurisuika@protonmail.com>
2024-04-27 16:02:40 -0700

    make REI plugin cycle through item stacks

--- src/main/java/dev/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
+++ src/main/java/dev/yurisuika/compost/integration/roughlyenoughitems/CompostClientPlugin.java
@@ -5,6 +5,7 @@ import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
 import me.shedaniel.rei.api.common.util.EntryIngredients;
+import me.shedaniel.rei.api.common.util.EntryStacks;
 import me.shedaniel.rei.plugin.common.displays.DefaultCompostingDisplay;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -19,12 +20,16 @@ public class CompostClientPlugin implements REIClientPlugin {
 
     @Override
     public void registerDisplays(DisplayRegistry registry) {
+        EntryIngredient.Builder output = EntryIngredient.builder();
+
         Arrays.stream(config.items).forEach(group -> {
-            Iterator<List<EntryIngredient>> iterator = Iterators.partition(ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.object2FloatEntrySet().stream().sorted(Map.Entry.comparingByValue()).map(entry -> EntryIngredients.of(entry.getKey())).iterator(), 35);
-            while (iterator.hasNext()) {
-                registry.add(new DefaultCompostingDisplay(iterator.next(), Collections.singletonList(EntryIngredients.of(createItemStack(group)))));
-            }
+            output.add(EntryStacks.of(createItemStack(group)));
         });
+
+        Iterator<List<EntryIngredient>> iterator = Iterators.partition(ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.object2FloatEntrySet().stream().sorted(Map.Entry.comparingByValue()).map(entry -> EntryIngredients.of(entry.getKey())).iterator(), 35);
+        while (iterator.hasNext()) {
+            registry.add(new DefaultCompostingDisplay(iterator.next(), List.of(output.build())));
+        }
     }
 
 }
