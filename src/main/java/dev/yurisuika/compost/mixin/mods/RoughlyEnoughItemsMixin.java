f8c3f270db3300cd3bbc57ca8802450b7f61a0ae
yurisuika <yurisuika@protonmail.com>
2024-04-28 09:48:33 -0700

    consolidate REI support to just mixin + average count

--- src/main/java/dev/yurisuika/compost/mixin/mods/RoughlyEnoughItemsMixin.java
+++ src/main/java/dev/yurisuika/compost/mixin/mods/RoughlyEnoughItemsMixin.java
@@ -1,13 +1,19 @@
 package dev.yurisuika.compost.mixin.mods;
 
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.util.EntryStacks;
 import me.shedaniel.rei.plugin.client.DefaultClientPlugin;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
 
-import java.util.Iterator;
+import java.util.Arrays;
+import java.util.List;
+
+import static dev.yurisuika.compost.client.option.CompostConfig.*;
 
 public abstract class RoughlyEnoughItemsMixin {
 
@@ -16,9 +22,17 @@ public abstract class RoughlyEnoughItemsMixin {
     @Mixin(DefaultClientPlugin.class)
     public abstract static class DefaultClientPluginMixin {
 
-        @Redirect(method = "registerDisplays", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/block/ComposterBlock;registerDefaultCompostableItems()V"), to = @At(value = "INVOKE", target = "Lme/shedaniel/rei/plugin/client/DefaultClientPlugin$DummyAxeItem;getStrippedBlocksMap()Ljava/util/Map;")))
-        private boolean redirectRegisterDefaultCompostableItems(Iterator instance) {
-            return false;
+        @Redirect(method = "registerDisplays", at = @At(value = "INVOKE", target = "Ljava/util/Collections;singletonList(Ljava/lang/Object;)Ljava/util/List;"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/block/ComposterBlock;registerDefaultCompostableItems()V"), to = @At(value = "INVOKE", target = "Lme/shedaniel/rei/plugin/client/DefaultClientPlugin$DummyAxeItem;getStrippedBlocksMap()Ljava/util/Map;")))
+        private <T> List<EntryIngredient> redirectCompostingOutput(T o) {
+            EntryIngredient.Builder output = EntryIngredient.builder();
+
+            Arrays.stream(config.items).forEach(group -> {
+                ItemStack stack = createItemStack(group);
+                stack.setCount((group.min + group.max) / 2);
+                output.add(EntryStacks.of(stack));
+            });
+
+            return List.of(output.build());
         }
 
     }
