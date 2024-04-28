15a7e7b2fdf97bdb3a0daff3871df12bdb930b2e
yurisuika <yurisuika@protonmail.com>
2024-04-28 09:47:23 -0700

    consolidate REI support to just mixin + average count

--- src/main/java/dev/yurisuika/compost/mixin/mods/RoughlyEnoughItemsMixin.java
+++ src/main/java/dev/yurisuika/compost/mixin/mods/RoughlyEnoughItemsMixin.java
@@ -1,13 +1,20 @@
 package dev.yurisuika.compost.mixin.mods;
 
 import me.shedaniel.rei.plugin.DefaultPlugin;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Slice;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static dev.yurisuika.compost.client.option.CompostConfig.*;
 
 public abstract class RoughlyEnoughItemsMixin {
 
@@ -15,9 +22,17 @@ public abstract class RoughlyEnoughItemsMixin {
     @Mixin(DefaultPlugin.class)
     public abstract static class DefaultPluginMixin {
 
-        @Redirect(method = "registerRecipeDisplays", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/block/ComposterBlock;registerDefaultCompostableItems()V"), to = @At(value = "INVOKE", target = "Lme/shedaniel/rei/plugin/stripping/DummyAxeItem;getStrippedBlocksMap()Ljava/util/Map;")))
-        private boolean redirectRegisterDefaultCompostableItems(Iterator instance) {
-            return false;
+        @ModifyArg(method = "registerRecipeDisplays", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;<init>(Lnet/minecraft/item/ItemConvertible;)V"), index = 0, slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/block/ComposterBlock;registerDefaultCompostableItems()V"), to = @At(value = "INVOKE", target = "Lme/shedaniel/rei/plugin/stripping/DummyAxeItem;getStrippedBlocksMap()Ljava/util/Map;")))
+        private ItemConvertible redirectCompostingOutput(ItemConvertible item) {
+            List<ItemStack> output = new ArrayList<>();
+
+            Arrays.stream(config.items).forEach(group -> {
+                ItemStack stack = createItemStack(group);
+                stack.setCount((group.min + group.max) / 2);
+                output.add(stack);
+            });
+
+            return output.get(ThreadLocalRandom.current().nextInt(output.size())).getItem();
         }
 
     }
