1c5f16150f79dfba10de1279a88f8ecb0ad8f3e2
yurisuika <yurisuika@protonmail.com>
2024-05-07 05:44:11 -0700

    multiple world config support + networking support

--- src/main/java/dev/yurisuika/compost/Compost.java
+++ src/main/java/dev/yurisuika/compost/Compost.java
@@ -2,8 +2,11 @@ package dev.yurisuika.compost;
 
 import dev.yurisuika.compost.block.entity.ComposterBlockEntity;
 import dev.yurisuika.compost.server.command.CompostCommand;
+import dev.yurisuika.compost.util.ConfigUtil;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntityType;
@@ -11,7 +14,9 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
-import static dev.yurisuika.compost.client.option.CompostConfig.*;
+import static dev.yurisuika.compost.server.option.CompostConfig.*;
+import static dev.yurisuika.compost.util.NetworkUtil.*;
+
 
 public class Compost implements ModInitializer {
 
@@ -29,6 +34,11 @@ public class Compost implements ModInitializer {
         CommandRegistrationCallback.EVENT.register(CompostCommand::register);
     }
 
+    public static void registerServerEvents() {
+        ServerLifecycleEvents.SERVER_STARTED.register(ConfigUtil::checkWorlds);
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> sendGroups(handler.getPlayer().getWorld(), handler.getPlayer()));
+    }
+
     @Override
     public void onInitialize() {
         if (!file.exists()) {
@@ -38,6 +48,7 @@ public class Compost implements ModInitializer {
 
         registerBlockEntityTypes();
         registerCommands();
+        registerServerEvents();
     }
 
 }
