986f94328096aff03b9a9b04b38d856f1dc6ae48
yurisuika <yurisuika@protonmail.com>
2024-05-09 23:39:05 -0700

    reorganize config

--- src/main/java/dev/yurisuika/compost/mixin/mods/RoughlyEnoughItemsMixin.java
+++ src/main/java/dev/yurisuika/compost/mixin/mods/RoughlyEnoughItemsMixin.java
@@ -1,20 +1,16 @@
 package dev.yurisuika.compost.mixin.mods;
 
+import dev.yurisuika.compost.util.NetworkUtil;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import me.shedaniel.rei.plugin.client.DefaultClientPlugin;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
 
-import java.util.Arrays;
 import java.util.List;
-
-import static dev.yurisuika.compost.CompostClient.*;
-import static dev.yurisuika.compost.util.ConfigUtil.*;
 
 public abstract class RoughlyEnoughItemsMixin {
 
@@ -26,11 +22,7 @@ public abstract class RoughlyEnoughItemsMixin {
         @Redirect(method = "registerDisplays", at = @At(value = "INVOKE", target = "Ljava/util/Collections;singletonList(Ljava/lang/Object;)Ljava/util/List;"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/block/ComposterBlock;registerDefaultCompostableItems()V"), to = @At(value = "INVOKE", target = "Lme/shedaniel/rei/plugin/client/DefaultClientPlugin$DummyAxeItem;getStrippedBlocksMap()Ljava/util/Map;")))
         private <T> List<EntryIngredient> redirectCompostingOutput(T o) {
             EntryIngredient.Builder output = EntryIngredient.builder();
-            Arrays.stream(GROUPS).forEach(group -> {
-                ItemStack stack = createItemStack(group);
-                stack.setCount((group.min + group.max) / 2);
-                output.add(EntryStacks.of(stack));
-            });
+            NetworkUtil.stacks.forEach(stack -> output.add(EntryStacks.of(stack)));
             return List.of(output.build());
         }
 
