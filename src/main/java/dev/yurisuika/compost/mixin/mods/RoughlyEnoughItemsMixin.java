7157ea94ae8eecb018246c12136623a76a5ebea3
yurisuika <yurisuika@protonmail.com>
2024-05-09 23:38:09 -0700

    reorganize config

--- src/main/java/dev/yurisuika/compost/mixin/mods/RoughlyEnoughItemsMixin.java
+++ src/main/java/dev/yurisuika/compost/mixin/mods/RoughlyEnoughItemsMixin.java
@@ -1,5 +1,6 @@
 package dev.yurisuika.compost.mixin.mods;
 
+import dev.yurisuika.compost.util.NetworkUtil;
 import me.shedaniel.rei.plugin.DefaultPlugin;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
@@ -10,12 +11,8 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Slice;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-
-import static dev.yurisuika.compost.CompostClient.*;
-import static dev.yurisuika.compost.util.ConfigUtil.*;
 
 public abstract class RoughlyEnoughItemsMixin {
 
@@ -26,11 +23,7 @@ public abstract class RoughlyEnoughItemsMixin {
         @ModifyArg(method = "registerRecipeDisplays", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;<init>(Lnet/minecraft/item/ItemConvertible;)V"), index = 0, slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/block/ComposterBlock;registerDefaultCompostableItems()V"), to = @At(value = "INVOKE", target = "Lme/shedaniel/rei/plugin/stripping/DummyAxeItem;getStrippedBlocksMap()Ljava/util/Map;")))
         private ItemConvertible redirectCompostingOutput(ItemConvertible item) {
             List<ItemStack> output = new ArrayList<>();
-            Arrays.stream(GROUPS).forEach(group -> {
-                ItemStack stack = createItemStack(group);
-                stack.setCount((group.min + group.max) / 2);
-                output.add(stack);
-            });
+            NetworkUtil.stacks.forEach(stack -> output.add(stack));
             return output.get(ThreadLocalRandom.current().nextInt(output.size())).getItem();
         }
 
