17f37058bdac006ec32db5ae1126b28137738a7e
yurisuika <yurisuika@protonmail.com>
2024-08-02 17:50:35 -0700

    mojmap + other changes too many to remember

---
+++ src/main/java/dev/yurisuika/compost/network/handler/ResetHandler.java
@@ -0,0 +1,21 @@
+package dev.yurisuika.compost.network.handler;
+
+import dev.yurisuika.compost.network.protocol.common.ClientboundResetPacket;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
+
+public class ResetHandler {
+
+    public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder
+            .named(ResourceLocation.tryParse("compost:reset"))
+            .networkProtocolVersion(() -> "1")
+            .clientAcceptedVersions(s -> true)
+            .serverAcceptedVersions(s -> true)
+            .simpleChannel();
+
+    public static void register() {
+        CHANNEL.registerMessage(1, ClientboundResetPacket.class, ClientboundResetPacket::encode, ClientboundResetPacket::decode, ClientboundResetPacket::handle);
+    }
+
+}
---
+++ src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundResetPacket.java
@@ -0,0 +1,25 @@
+package dev.yurisuika.compost.network.protocol.common;
+
+import dev.yurisuika.compost.util.Network;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
+public record ClientboundResetPacket() {
+
+    public static void encode(ClientboundResetPacket packet, FriendlyByteBuf buffer) {}
+
+    public static ClientboundResetPacket decode(FriendlyByteBuf buffer) {
+        return new ClientboundResetPacket();
+    }
+
+    public static void handle(final ClientboundResetPacket message, Supplier<NetworkEvent.Context> context) {
+        context.get().enqueueWork(() -> {
+            Network.setStacks(new ArrayList<>());
+        });
+        context.get().setPacketHandled(true);
+    }
+
+}
