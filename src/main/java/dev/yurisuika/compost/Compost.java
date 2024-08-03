fb9bc61cc2840e071760d20f395a675a30dfff01
yurisuika <yurisuika@protonmail.com>
2024-08-02 17:50:47 -0700

    mojmap + other changes too many to remember

--- src/main/java/dev/yurisuika/compost/Compost.java
+++ src/main/java/dev/yurisuika/compost/Compost.java
@@ -1,13 +1,19 @@
 package dev.yurisuika.compost;
 
-import dev.yurisuika.compost.block.entity.ComposterBlockEntity;
-import dev.yurisuika.compost.network.handler.CompostHandler;
-import dev.yurisuika.compost.server.command.CompostCommand;
-import dev.yurisuika.compost.util.ConfigUtil;
-import dev.yurisuika.compost.util.CompostUtil;
-import dev.yurisuika.compost.util.NetworkUtil;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.entity.BlockEntityType;
+import dev.yurisuika.compost.commands.arguments.ProduceArgument;
+import dev.yurisuika.compost.network.handler.ProduceHandler;
+import dev.yurisuika.compost.network.handler.ResetHandler;
+import dev.yurisuika.compost.server.commands.CompostCommand;
+import dev.yurisuika.compost.util.Network;
+import dev.yurisuika.compost.util.Validate;
+import dev.yurisuika.compost.util.config.Config;
+import dev.yurisuika.compost.world.level.block.entity.ContainerComposterBlockEntity;
+import net.minecraft.commands.synchronization.ArgumentTypeInfos;
+import net.minecraft.commands.synchronization.SingletonArgumentInfo;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
@@ -18,6 +24,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Objects;
@@ -27,7 +34,7 @@ public class Compost {
 
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, "compost");
 
-    public static final RegistryObject<BlockEntityType<ComposterBlockEntity>> COMPOSTER = BLOCK_ENTITIES.register("composter", () -> BlockEntityType.Builder.create(ComposterBlockEntity::new, Blocks.COMPOSTER).build(null));
+    public static final RegistryObject<BlockEntityType<ContainerComposterBlockEntity>> COMPOSTER = BLOCK_ENTITIES.register("composter", () -> BlockEntityType.Builder.of(ContainerComposterBlockEntity::new, Blocks.COMPOSTER).build(null));
 
     @Mod.EventBusSubscriber(modid = "compost")
     public static class CommonForgeEvents {
@@ -39,12 +46,12 @@ public class Compost {
 
         @SubscribeEvent
         public static void serverStartedEvents(ServerStartedEvent event) {
-            CompostUtil.checkLevels(Objects.requireNonNull(event.getServer()));
+            Validate.checkLevels(Objects.requireNonNull(event.getServer()));
         }
 
         @SubscribeEvent
         public static void playerLoggedInEvents(PlayerEvent.PlayerLoggedInEvent event) {
-            NetworkUtil.sendItems(event.getEntity().getWorld(), event.getEntity());
+            Network.sendProduce(event.getEntity().level(), event.getEntity());
         }
 
     }
@@ -54,13 +61,19 @@ public class Compost {
 
         @SubscribeEvent
         public static void commonSetup(FMLCommonSetupEvent event) {
-            CompostHandler.register();
+            ProduceHandler.register();
+            ResetHandler.register();
+        }
+
+        @SubscribeEvent
+        public static void registerCommandArgumentTypes(RegisterEvent event) {
+            event.register(Registries.COMMAND_ARGUMENT_TYPE, ResourceLocation.tryParse("compost:produce"), () -> ArgumentTypeInfos.registerByClass(ProduceArgument.class, SingletonArgumentInfo.contextFree(ProduceArgument::produce)));
         }
 
     }
 
     public Compost() {
-        ConfigUtil.loadConfig();
+        Config.loadConfig();
 
         MinecraftForge.EVENT_BUS.register(this);
 
