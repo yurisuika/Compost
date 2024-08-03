0ce1680a05f29afd3b520225b0c5c928915e323d
yurisuika <yurisuika@protonmail.com>
2024-08-02 17:49:05 -0700

    mojmap + other changes too many to remember

--- src/main/java/dev/yurisuika/compost/mixin/mods/RoughlyEnoughItemsMixin.java
+++ src/main/java/dev/yurisuika/compost/mixin/mods/RoughlyEnoughItemsMixin.java
@@ -1,6 +1,6 @@
 package dev.yurisuika.compost.mixin.mods;
 
-import dev.yurisuika.compost.util.NetworkUtil;
+import dev.yurisuika.compost.util.Network;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import me.shedaniel.rei.plugin.client.DefaultClientPlugin;
@@ -19,10 +19,10 @@ public abstract class RoughlyEnoughItemsMixin {
     @Mixin(DefaultClientPlugin.class)
     public abstract static class DefaultClientPluginMixin {
 
-        @Redirect(method = "registerDisplays", at = @At(value = "INVOKE", target = "Ljava/util/Collections;singletonList(Ljava/lang/Object;)Ljava/util/List;"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/block/ComposterBlock;registerDefaultCompostableItems()V"), to = @At(value = "INVOKE", target = "Lme/shedaniel/rei/plugin/client/DefaultClientPlugin$DummyAxeItem;getStrippedBlocksMap()Ljava/util/Map;")))
+        @Redirect(method = "registerDisplays", at = @At(value = "INVOKE", target = "Ljava/util/Collections;singletonList(Ljava/lang/Object;)Ljava/util/List;"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/ComposterBlock;bootStrap()V"), to = @At(value = "INVOKE", target = "Lme/shedaniel/rei/plugin/client/DefaultClientPlugin$DummyAxeItem;getStrippedBlocksMap()Ljava/util/Map;")))
         private <T> List<EntryIngredient> redirectCompostingOutput(T o) {
             EntryIngredient.Builder output = EntryIngredient.builder();
-            NetworkUtil.stacks.forEach(stack -> output.add(EntryStacks.of(stack)));
+            Network.getStacks().forEach(stack -> output.add(EntryStacks.of(stack)));
             return List.of(output.build());
         }
 
