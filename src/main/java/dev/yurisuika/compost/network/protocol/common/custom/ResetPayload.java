9d0e0fd61cc6e1d1056a2061af068d0285b9a175
yurisuika <yurisuika@protonmail.com>
2024-08-10 22:25:02 -0700

    use new Type for compost namespace

--- src/main/java/dev/yurisuika/compost/network/protocol/common/custom/ResetPayload.java
+++ src/main/java/dev/yurisuika/compost/network/protocol/common/custom/ResetPayload.java
@@ -3,11 +3,12 @@ package dev.yurisuika.compost.network.protocol.common.custom;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.resources.ResourceLocation;
 
 public record ResetPayload() implements CustomPacketPayload {
 
     public static final StreamCodec<FriendlyByteBuf, ResetPayload> STREAM_CODEC = StreamCodec.unit(new ResetPayload());
-    public static final Type<ResetPayload> TYPE = CustomPacketPayload.createType("compost:reset");
+    public static final Type<ResetPayload> TYPE = new Type<>(ResourceLocation.tryParse("compost:reset"));
 
     @Override
     public Type<ResetPayload> type() {
