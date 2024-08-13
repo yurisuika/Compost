c203b45267ff9ff897761878f8437a32465fe1cf
yurisuika <yurisuika@protonmail.com>
2024-08-12 18:18:17 -0700

    packet parity

--- src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundProducePacket.java
+++ src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundProducePacket.java
@@ -4,16 +4,21 @@ import dev.yurisuika.compost.util.Network;
 import dev.yurisuika.compost.util.Parse;
 import dev.yurisuika.compost.util.config.options.Produce;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 import java.util.function.Supplier;
 
 public final class ClientboundProducePacket {
 
-    public final String item;
-    public final Double chance;
-    public final Integer min;
-    public final Integer max;
+    public String item;
+    public Double chance;
+    public Integer min;
+    public Integer max;
+    public static final ResourceLocation ID = ResourceLocation.tryParse("compost:produce");
+    public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder.named(ID).networkProtocolVersion(() -> "1").clientAcceptedVersions(s -> true).serverAcceptedVersions(s -> true).simpleChannel();
 
     public ClientboundProducePacket(String item, Double chance, Integer min, Integer max) {
         this.item = item;
@@ -22,21 +27,26 @@ public final class ClientboundProducePacket {
         this.max = max;
     }
 
-    public static void encode(ClientboundProducePacket packet, FriendlyByteBuf buffer) {
-        buffer.writeUtf(packet.item);
-        buffer.writeDouble(packet.chance);
-        buffer.writeInt(packet.min);
-        buffer.writeInt(packet.max);
+    public ClientboundProducePacket(FriendlyByteBuf buffer) {
+        this(buffer.readUtf(), buffer.readDouble(), buffer.readInt(), buffer.readInt());
     }
 
-    public static ClientboundProducePacket decode(FriendlyByteBuf buffer) {
-        return new ClientboundProducePacket(buffer.readUtf(), buffer.readDouble(), buffer.readInt(), buffer.readInt());
+    public void read(FriendlyByteBuf buffer) {
+        this.item = buffer.readUtf();
+        this.chance = buffer.readDouble();
+        this.min = buffer.readInt();
+        this.max = buffer.readInt();
     }
 
-    public static void handle(final ClientboundProducePacket message, Supplier<NetworkEvent.Context> context) {
-        context.get().enqueueWork(() -> {
-            Network.getStacks().add(Parse.createItemStack(new Produce(message.item(), message.chance(), message.min(), message.max())));
-        });
+    public static void write(ClientboundProducePacket packet, FriendlyByteBuf buffer) {
+        buffer.writeUtf(packet.item());
+        buffer.writeDouble(packet.chance());
+        buffer.writeInt(packet.min());
+        buffer.writeInt(packet.max());
+    }
+
+    public static void handle(ClientboundProducePacket packet, Supplier<NetworkEvent.Context> context) {
+        context.get().enqueueWork(() -> Network.getStacks().add(Parse.createItemStack(new Produce(packet.item(), packet.chance(), packet.min(), packet.max()))));
         context.get().setPacketHandled(true);
     }
 
--- src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundResetPacket.java
+++ src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundResetPacket.java
@@ -2,25 +2,31 @@ package dev.yurisuika.compost.network.protocol.common;
 
 import dev.yurisuika.compost.util.Network;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 import java.util.ArrayList;
 import java.util.function.Supplier;
 
 public final class ClientboundResetPacket {
 
+    public static final ResourceLocation ID = ResourceLocation.tryParse("compost:reset");
+    public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder.named(ID).networkProtocolVersion(() -> "1").clientAcceptedVersions(s -> true).serverAcceptedVersions(s -> true).simpleChannel();
+
     public ClientboundResetPacket() {}
 
-    public static void encode(ClientboundResetPacket packet, FriendlyByteBuf buffer) {}
-
-    public static ClientboundResetPacket decode(FriendlyByteBuf buffer) {
-        return new ClientboundResetPacket();
+    public ClientboundResetPacket(FriendlyByteBuf buffer) {
+        this();
     }
 
-    public static void handle(final ClientboundResetPacket message, Supplier<NetworkEvent.Context> context) {
-        context.get().enqueueWork(() -> {
-            Network.setStacks(new ArrayList<>());
-        });
+    public void read(FriendlyByteBuf buffer) {}
+
+    public static void write(ClientboundResetPacket packet, FriendlyByteBuf buffer) {}
+
+    public static void handle(ClientboundResetPacket packet, Supplier<NetworkEvent.Context> context) {
+        context.get().enqueueWork(() -> Network.setStacks(new ArrayList<>()));
         context.get().setPacketHandled(true);
     }
 
