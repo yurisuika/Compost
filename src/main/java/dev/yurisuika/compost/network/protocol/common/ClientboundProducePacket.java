465ed8feff72ad13d219e356b7311efbe8da9d5a
yurisuika <yurisuika@protonmail.com>
2024-08-12 18:19:18 -0700

    packet parity

--- src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundProducePacket.java
+++ src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundProducePacket.java
@@ -4,25 +4,29 @@ import dev.yurisuika.compost.util.Network;
 import dev.yurisuika.compost.util.Parse;
 import dev.yurisuika.compost.util.config.options.Produce;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.event.network.CustomPayloadEvent;
+import net.minecraftforge.network.ChannelBuilder;
+import net.minecraftforge.network.SimpleChannel;
 
 public record ClientboundProducePacket(String item, Double chance, Integer min, Integer max) {
 
-    public static void encode(ClientboundProducePacket packet, FriendlyByteBuf buffer) {
-        buffer.writeUtf(packet.item);
-        buffer.writeDouble(packet.chance);
-        buffer.writeInt(packet.min);
-        buffer.writeInt(packet.max);
+    public static final ResourceLocation ID = ResourceLocation.tryParse("compost:produce");
+    public static final SimpleChannel CHANNEL = ChannelBuilder.named(ID).optional().networkProtocolVersion(1).acceptedVersions((status, version) -> true).simpleChannel();
+
+    public ClientboundProducePacket(FriendlyByteBuf buffer) {
+        this(buffer.readUtf(), buffer.readDouble(), buffer.readInt(), buffer.readInt());
     }
 
-    public static ClientboundProducePacket decode(FriendlyByteBuf buffer) {
-        return new ClientboundProducePacket(buffer.readUtf(), buffer.readDouble(), buffer.readInt(), buffer.readInt());
+    public static void write(ClientboundProducePacket packet, FriendlyByteBuf buffer) {
+        buffer.writeUtf(packet.item());
+        buffer.writeDouble(packet.chance());
+        buffer.writeInt(packet.min());
+        buffer.writeInt(packet.max());
     }
 
-    public static void handle(final ClientboundProducePacket message, CustomPayloadEvent.Context context) {
-        context.enqueueWork(() -> {
-            Network.getStacks().add(Parse.createItemStack(context.getSender().getServer().registryAccess(), new Produce(message.item(), message.chance(), message.min(), message.max())));
-        });
+    public static void handle(ClientboundProducePacket packet, CustomPayloadEvent.Context context) {
+        context.enqueueWork(() -> Network.getStacks().add(Parse.createItemStack(context.getSender().getServer().registryAccess(), new Produce(packet.item(), packet.chance(), packet.min(), packet.max()))));
         context.setPacketHandled(true);
     }
 
