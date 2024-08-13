027352afc4b797a3fbce7608160b9879a35edf7c
yurisuika <yurisuika@protonmail.com>
2024-08-12 18:17:55 -0700

    packet parity

--- src/main/java/dev/yurisuika/compost/Compost.java
+++ src/main/java/dev/yurisuika/compost/Compost.java
@@ -1,12 +1,12 @@
 package dev.yurisuika.compost;
 
 import dev.yurisuika.compost.commands.arguments.ProduceArgument;
+import dev.yurisuika.compost.network.protocol.common.ClientboundProducePacket;
+import dev.yurisuika.compost.network.protocol.common.ClientboundResetPacket;
 import dev.yurisuika.compost.server.commands.CompostCommand;
 import dev.yurisuika.compost.util.Network;
-import dev.yurisuika.compost.util.Parse;
 import dev.yurisuika.compost.util.Validate;
 import dev.yurisuika.compost.util.config.Config;
-import dev.yurisuika.compost.util.config.options.Produce;
 import dev.yurisuika.compost.world.level.block.entity.ContainerComposterBlockEntity;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
@@ -23,18 +23,12 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
-import java.util.ArrayList;
-
 public class Compost implements ModInitializer {
 
     public static BlockEntityType<ContainerComposterBlockEntity> COMPOSTER;
 
     public static void registerBlockEntityTypes() {
-        COMPOSTER = Registry.register(
-                BuiltInRegistries.BLOCK_ENTITY_TYPE,
-                ResourceLocation.tryParse("compost:composter"),
-                FabricBlockEntityTypeBuilder.create(ContainerComposterBlockEntity::new, Blocks.COMPOSTER).build()
-        );
+        COMPOSTER = Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE, ResourceLocation.tryParse("compost:composter"), FabricBlockEntityTypeBuilder.create(ContainerComposterBlockEntity::new, Blocks.COMPOSTER).build());
     }
 
     public static void registerServerEvents() {
@@ -58,12 +52,12 @@ public class Compost implements ModInitializer {
     public static class Client implements ClientModInitializer {
 
         public static void registerArgumentTypes() {
-            ArgumentTypeRegistry.registerArgumentType(ResourceLocation.tryParse("compost:produce"), ProduceArgument.class, SingletonArgumentInfo.contextFree(ProduceArgument::produce));
+            ArgumentTypeRegistry.registerArgumentType(ProduceArgument.ID, ProduceArgument.class, SingletonArgumentInfo.contextFree(ProduceArgument::produce));
         }
 
         public static void registerGlobalReceivers() {
-            ClientPlayNetworking.registerGlobalReceiver(ResourceLocation.tryParse("compost:produce"), (minecraft, listener, buffer, sender) -> Network.getStacks().add(Parse.createItemStack(new Produce(buffer.readUtf(), buffer.readDouble(), buffer.readInt(), buffer.readInt()))));
-            ClientPlayNetworking.registerGlobalReceiver(ResourceLocation.tryParse("compost:reset"), (minecraft, listener, buffer, sender) -> Network.setStacks(new ArrayList<>()));
+            ClientPlayNetworking.registerGlobalReceiver(ClientboundProducePacket.ID, ClientboundProducePacket::handle);
+            ClientPlayNetworking.registerGlobalReceiver(ClientboundResetPacket.ID, ClientboundResetPacket::handle);
         }
 
         @Override
@@ -73,4 +67,5 @@ public class Compost implements ModInitializer {
         }
 
     }
+
 }
---
+++ src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundResetPacket.java
@@ -0,0 +1,35 @@
+package dev.yurisuika.compost.network.protocol.common;
+
+import dev.yurisuika.compost.util.Network;
+import net.fabricmc.fabric.api.networking.v1.FabricPacket;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.fabric.api.networking.v1.PacketType;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.ArrayList;
+
+public record ClientboundResetPacket() implements FabricPacket {
+
+    public static final ResourceLocation ID = ResourceLocation.tryParse("compost:reset");
+    public static final PacketType<ClientboundResetPacket> TYPE = PacketType.create(ID, ClientboundResetPacket::new);
+
+    public ClientboundResetPacket(FriendlyByteBuf buffer) {
+        this();
+    }
+
+    @Override
+    public void write(FriendlyByteBuf buffer) {}
+
+    public static void handle(Minecraft minecraft, ClientPacketListener listener, FriendlyByteBuf buffer, PacketSender sender) {
+        Network.setStacks(new ArrayList<>());
+    }
+
+    @Override
+    public PacketType<ClientboundResetPacket> getType() {
+        return TYPE;
+    }
+
+}
