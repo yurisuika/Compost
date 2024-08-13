1b7d87957277d0d44485963421607dffcd3b2bf8
yurisuika <yurisuika@protonmail.com>
2024-08-12 18:17:35 -0700

    packet parity

---
+++ src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundResetPacket.java
@@ -0,0 +1,26 @@
+package dev.yurisuika.compost.network.protocol.common;
+
+import dev.yurisuika.compost.util.Network;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.ArrayList;
+
+public record ClientboundResetPacket() {
+
+    public static final ResourceLocation ID = ResourceLocation.tryParse("compost:reset");
+
+    public ClientboundResetPacket(FriendlyByteBuf buffer) {
+        this();
+    }
+
+    public void write(FriendlyByteBuf buffer) {}
+
+    public static void handle(Minecraft minecraft, ClientPacketListener listener, FriendlyByteBuf buffer, PacketSender sender) {
+        Network.setStacks(new ArrayList<>());
+    }
+
+}
