77beb1c884f3adffc90b6ead77791989cba3485d
yurisuika <yurisuika@protonmail.com>
2024-08-12 18:17:29 -0700

    packet parity

--- src/main/java/dev/yurisuika/compost/util/Network.java
+++ src/main/java/dev/yurisuika/compost/util/Network.java
@@ -1,10 +1,11 @@
 package dev.yurisuika.compost.util;
 
+import dev.yurisuika.compost.network.protocol.common.ClientboundProducePacket;
+import dev.yurisuika.compost.network.protocol.common.ClientboundResetPacket;
 import dev.yurisuika.compost.util.config.Option;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -28,17 +29,16 @@ public class Network {
 
     public static void sendProduce(Level level, Player player) {
         if (!level.isClientSide()) {
-            FriendlyByteBuf reset = PacketByteBufs.create();
-            ServerPlayNetworking.send((ServerPlayer) player, ResourceLocation.tryParse("compost:reset"), reset);
+            ServerPlayNetworking.send((ServerPlayer) player, ClientboundResetPacket.ID, PacketByteBufs.empty());
             Option.getWorlds().forEach(world -> {
-                if (Objects.equals(world.getName(), Objects.requireNonNull(level.getServer()).getWorldData().getLevelName())) {
+                if (Objects.equals(world.getName(), level.getServer().getWorldData().getLevelName())) {
                     world.getProduce().forEach(produce -> {
                         FriendlyByteBuf buffer = PacketByteBufs.create();
                         buffer.writeUtf(produce.getItem());
                         buffer.writeDouble(produce.getChance());
                         buffer.writeInt(produce.getMin());
                         buffer.writeInt(produce.getMax());
-                        ServerPlayNetworking.send((ServerPlayer) player, ResourceLocation.tryParse("compost:produce"), buffer);
+                        ServerPlayNetworking.send((ServerPlayer) player, ClientboundProducePacket.ID, buffer);
                     });
                 }
             });
