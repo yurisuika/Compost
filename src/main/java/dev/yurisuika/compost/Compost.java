86ec7a43f2ffbd03b06013a8a14b961d95049556
yurisuika <yurisuika@protonmail.com>
2024-10-11 01:15:21 -0700

    #11

--- src/main/java/dev/yurisuika/compost/Compost.java
+++ src/main/java/dev/yurisuika/compost/Compost.java
@@ -36,6 +36,10 @@ public class Compost implements ModInitializer {
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> Network.sendProduce(handler.getPlayer().getLevel(), handler.getPlayer()));
     }
 
+    public static void registerArgumentTypes() {
+        ArgumentTypeRegistry.registerArgumentType(ResourceLocation.tryParse("compost:produce"), ProduceArgument.class, SingletonArgumentInfo.contextFree(ProduceArgument::produce));
+    }
+
     public static void registerCommands() {
         CommandRegistrationCallback.EVENT.register(CompostCommand::register);
     }
@@ -46,14 +50,11 @@ public class Compost implements ModInitializer {
 
         registerBlockEntityTypes();
         registerServerEvents();
+        registerArgumentTypes();
         registerCommands();
     }
 
     public static class Client implements ClientModInitializer {
-
-        public static void registerArgumentTypes() {
-            ArgumentTypeRegistry.registerArgumentType(ResourceLocation.tryParse("compost:produce"), ProduceArgument.class, SingletonArgumentInfo.contextFree(ProduceArgument::produce));
-        }
 
         public static void registerGlobalReceivers() {
             ClientPlayNetworking.registerGlobalReceiver(ClientboundProducePacket.ID, ClientboundProducePacket::handle);
@@ -62,7 +63,6 @@ public class Compost implements ModInitializer {
 
         @Override
         public void onInitializeClient() {
-            registerArgumentTypes();
             registerGlobalReceivers();
         }
 
