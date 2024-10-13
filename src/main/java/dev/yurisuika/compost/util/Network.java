75e4c7740293654e290d72a6d22bef1f4dedce8d
yurisuika <yurisuika@protonmail.com>
2024-10-13 02:51:01 -0700

    set stacks/produce on dedicated server

--- src/main/java/dev/yurisuika/compost/util/Network.java
+++ src/main/java/dev/yurisuika/compost/util/Network.java
@@ -38,9 +38,15 @@ public class Network {
     public static void sendProduce(Level level, Player player) {
         if (!level.isClientSide()) {
             ServerPlayNetworking.send((ServerPlayer) player, new ResetPayload());
+            setStacks(new ArrayList<>());
+            setProduce(new ArrayList<>());
             Option.getWorlds().forEach(world -> {
                 if (Objects.equals(world.getName(), level.getServer().getWorldData().getLevelName())) {
-                    world.getProduce().forEach(produce -> ServerPlayNetworking.send((ServerPlayer) player, new ProducePayload(produce.getItem(), produce.getChance(), produce.getMin(), produce.getMax())));
+                    world.getProduce().forEach(produce -> {
+                        ServerPlayNetworking.send((ServerPlayer) player, new ProducePayload(produce.getItem(), produce.getChance(), produce.getMin(), produce.getMax()));
+                        getStacks().add(Parse.createItemStack(level.getServer().registryAccess(), produce));
+                        getProduce().add(produce);
+                    });
                 }
             });
         }
