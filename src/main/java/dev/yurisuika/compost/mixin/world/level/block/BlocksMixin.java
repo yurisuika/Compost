9f9c75ae5351d1842a0c8427440a29f6ffaa7d9b
yurisuika <yurisuika@protonmail.com>
2024-12-27 01:21:43 -0800

    reorganize registration

--- src/main/java/dev/yurisuika/compost/mixin/world/level/block/BlocksMixin.java
+++ src/main/java/dev/yurisuika/compost/mixin/world/level/block/BlocksMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.Slice;
 public abstract class BlocksMixin {
 
     @Redirect(method = "<clinit>", slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=composter")), at = @At(value = "NEW", target = "net/minecraft/world/level/block/ComposterBlock", ordinal = 0))
-    private static ComposterBlock redirectComposter(Properties properties) {
+    private static ComposterBlock replaceComposter(Properties properties) {
         return new ContainerComposterBlock(Properties.of(Material.WOOD).strength(0.6F).sound(SoundType.WOOD));
     }
 
