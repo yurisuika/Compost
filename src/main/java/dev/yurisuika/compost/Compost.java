15df1b1684e26d09ca8a6cfb524003b7e5f54d5b
yurisuika <yurisuika@protonmail.com>
2024-12-27 01:22:26 -0800

    reorganize registration

--- src/main/java/dev/yurisuika/compost/Compost.java
+++ src/main/java/dev/yurisuika/compost/Compost.java
@@ -7,7 +7,7 @@ import dev.yurisuika.compost.server.commands.CompostCommand;
 import dev.yurisuika.compost.util.Network;
 import dev.yurisuika.compost.util.Validate;
 import dev.yurisuika.compost.util.config.Config;
-import dev.yurisuika.compost.world.level.block.entity.ContainerComposterBlockEntity;
+import dev.yurisuika.compost.world.level.block.entity.CompostBlockEntityType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
@@ -15,25 +15,15 @@ import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.commands.synchronization.SingletonArgumentInfo;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public class Compost implements ModInitializer {
 
-    public static BlockEntityType<ContainerComposterBlockEntity> COMPOSTER;
-
     public static void registerBlockEntityTypes() {
-        COMPOSTER = Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE, ResourceLocation.tryParse("compost:composter"), FabricBlockEntityTypeBuilder.create(ContainerComposterBlockEntity::new, Blocks.COMPOSTER).build());
-    }
-
-    public static void registerServerEvents() {
-        ServerLifecycleEvents.SERVER_STARTED.register(Validate::checkLevels);
-        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> Network.sendProduce(handler.getPlayer().level(), handler.getPlayer()));
+        Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE, ResourceLocation.tryParse("compost:composter"), CompostBlockEntityType.COMPOSTER);
     }
 
     public static void registerArgumentTypes() {
@@ -44,26 +34,35 @@ public class Compost implements ModInitializer {
         CommandRegistrationCallback.EVENT.register(CompostCommand::register);
     }
 
+    public static void registerLevelValidation() {
+        ServerLifecycleEvents.SERVER_STARTED.register(Validate::checkLevels);
+    }
+
+    public static void registerJoinPacket() {
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> Network.sendProduce(handler.getPlayer().level(), handler.getPlayer()));
+    }
+
     @Override
     public void onInitialize() {
         Config.loadConfig();
 
         registerBlockEntityTypes();
-        registerServerEvents();
         registerArgumentTypes();
         registerCommands();
+        registerLevelValidation();
+        registerJoinPacket();
     }
 
     public static class Client implements ClientModInitializer {
 
-        public static void registerGlobalReceivers() {
+        public static void registerClientReceivers() {
             ClientPlayNetworking.registerGlobalReceiver(ClientboundProducePacket.ID, ClientboundProducePacket::handle);
             ClientPlayNetworking.registerGlobalReceiver(ClientboundResetPacket.ID, ClientboundResetPacket::handle);
         }
 
         @Override
         public void onInitializeClient() {
-            registerGlobalReceivers();
+            registerClientReceivers();
         }
 
     }
