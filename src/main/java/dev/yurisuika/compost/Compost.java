bc710ee853730188a7313cdb82e86b41a34a2eb8
yurisuika <yurisuika@protonmail.com>
2024-12-27 01:23:01 -0800

    reorganize registration

--- src/main/java/dev/yurisuika/compost/Compost.java
+++ src/main/java/dev/yurisuika/compost/Compost.java
@@ -7,34 +7,26 @@ import dev.yurisuika.compost.server.commands.CompostCommand;
 import dev.yurisuika.compost.util.Network;
 import dev.yurisuika.compost.util.Validate;
 import dev.yurisuika.compost.util.config.Config;
-import dev.yurisuika.compost.world.level.block.entity.ContainerComposterBlockEntity;
+import dev.yurisuika.compost.world.level.block.entity.CompostBlockEntityType;
 import net.minecraft.commands.synchronization.ArgumentTypeInfos;
 import net.minecraft.commands.synchronization.SingletonArgumentInfo;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.server.ServerStartedEvent;
 import net.neoforged.neoforge.network.PlayNetworkDirection;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.RegisterEvent;
 
 @Mod("compost")
 public class Compost {
 
-    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, "compost");
-
-    public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ContainerComposterBlockEntity>> COMPOSTER = BLOCK_ENTITIES.register("composter", () -> BlockEntityType.Builder.of(ContainerComposterBlockEntity::new, Blocks.COMPOSTER).build(null));
-
-    @Mod.EventBusSubscriber(modid = "compost")
-    public static class CommonForgeEvents {
+    @Mod.EventBusSubscriber(modid = "compost", bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class GameEvents {
 
         @SubscribeEvent
         public static void registerCommands(RegisterCommandsEvent event) {
@@ -42,37 +34,40 @@ public class Compost {
         }
 
         @SubscribeEvent
-        public static void serverStartedEvents(ServerStartedEvent event) {
+        public static void registerLevelValidation(ServerStartedEvent event) {
             Validate.checkLevels(event.getServer());
         }
 
         @SubscribeEvent
-        public static void playerLoggedInEvents(PlayerEvent.PlayerLoggedInEvent event) {
+        public static void registerJoinPacket(PlayerEvent.PlayerLoggedInEvent event) {
             Network.sendProduce(event.getEntity().level(), event.getEntity());
         }
 
     }
 
     @Mod.EventBusSubscriber(modid = "compost", bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class CommonModBusEvents {
+    public static class ModEvents {
 
         @SubscribeEvent
-        public static void commonSetup(FMLCommonSetupEvent event) {
-            ClientboundProducePacket.CHANNEL.messageBuilder(ClientboundProducePacket.class, 1, PlayNetworkDirection.PLAY_TO_CLIENT).encoder(ClientboundProducePacket::write).decoder(ClientboundProducePacket::new).consumerMainThread(ClientboundProducePacket::handle).add();
-            ClientboundResetPacket.CHANNEL.messageBuilder(ClientboundResetPacket.class, 1, PlayNetworkDirection.PLAY_TO_CLIENT).encoder(ClientboundResetPacket::write).decoder(ClientboundResetPacket::new).consumerMainThread(ClientboundResetPacket::handle).add();
+        public static void registerBlockEntityTypes(RegisterEvent event) {
+            event.register(BuiltInRegistries.BLOCK_ENTITY_TYPE.key(), helper -> helper.register(ResourceLocation.tryParse("compost:composter"), CompostBlockEntityType.COMPOSTER));
         }
 
         @SubscribeEvent
-        public static void registerCommandArgumentTypes(RegisterEvent event) {
+        public static void registerArgumentTypes(RegisterEvent event) {
             event.register(Registries.COMMAND_ARGUMENT_TYPE, ResourceLocation.tryParse("compost:produce"), () -> ArgumentTypeInfos.registerByClass(ProduceArgument.class, SingletonArgumentInfo.contextFree(ProduceArgument::produce)));
+        }
+
+        @SubscribeEvent
+        public static void registerPackets(FMLCommonSetupEvent event) {
+            ClientboundProducePacket.CHANNEL.messageBuilder(ClientboundProducePacket.class, 1, PlayNetworkDirection.PLAY_TO_CLIENT).encoder(ClientboundProducePacket::write).decoder(ClientboundProducePacket::new).consumerMainThread(ClientboundProducePacket::handle).add();
+            ClientboundResetPacket.CHANNEL.messageBuilder(ClientboundResetPacket.class, 1, PlayNetworkDirection.PLAY_TO_CLIENT).encoder(ClientboundResetPacket::write).decoder(ClientboundResetPacket::new).consumerMainThread(ClientboundResetPacket::handle).add();
         }
 
     }
 
     public Compost() {
         Config.loadConfig();
-
-        BLOCK_ENTITIES.register(ModLoadingContext.get().getActiveContainer().getEventBus());
     }
 
 }
