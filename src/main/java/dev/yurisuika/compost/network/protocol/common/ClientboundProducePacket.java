ef6d1ecbbeac85d72da14c7f7873d3712b0ef36b
yurisuika <yurisuika@protonmail.com>
2025-04-22 20:20:46 -0700

    use VanillaRegistries for now until I figure out what alternative there is

--- src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundProducePacket.java
+++ src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundProducePacket.java
@@ -6,6 +6,7 @@ import dev.yurisuika.compost.util.config.options.Produce;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.data.registries.VanillaRegistries;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 
@@ -26,7 +27,7 @@ public record ClientboundProducePacket(String item, Double chance, Integer min, 
 
     public static void handle(Minecraft minecraft, ClientPacketListener listener, FriendlyByteBuf buffer, PacketSender sender) {
         Produce produce = new Produce(buffer.readUtf(), buffer.readDouble(), buffer.readInt(), buffer.readInt());
-        Network.getStacks().add(Parse.createItemStack(listener.getLevel().registryAccess(), produce));
+        Network.getStacks().add(Parse.createItemStack(VanillaRegistries.createLookup(), produce));
         Network.getProduce().add(produce);
     }
 
