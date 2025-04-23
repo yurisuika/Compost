f1f7a5b6fdc26973366bc50358504475db177d47
yurisuika <yurisuika@protonmail.com>
2025-04-22 18:42:12 -0700

    Ensure createItemStack is using registry access from level.

--- src/main/java/dev/yurisuika/compost/util/Network.java
+++ src/main/java/dev/yurisuika/compost/util/Network.java
@@ -44,7 +44,7 @@ public class Network {
                 if (Objects.equals(world.getName(),level.getServer().getWorldData().getLevelName())) {
                     world.getProduce().forEach(produce -> {
                         ClientboundProducePacket.CHANNEL.send(new ClientboundProducePacket(produce.getItem(), produce.getChance(), produce.getMin(), produce.getMax()), PacketDistributor.PLAYER.with((ServerPlayer) player));
-                        getStacks().add(Parse.createItemStack(produce));
+                        getStacks().add(Parse.createItemStack(level.getServer().registryAccess(), produce));
                         getProduce().add(produce);
                     });
                 }
