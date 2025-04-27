6310abee6aea5c059f54abb3c4260f2d57e52245
yurisuika <yurisuika@protonmail.com>
2025-04-26 21:59:25 -0700

    Document this one change before the big rewrite erases it. Fixed remove command looking for the wrong produce object.

--- src/main/java/dev/yurisuika/compost/api/CompostApi.java
+++ src/main/java/dev/yurisuika/compost/api/CompostApi.java
@@ -1,9 +1,7 @@
 package dev.yurisuika.compost.api;
 
-import dev.yurisuika.compost.util.Network;
 import dev.yurisuika.compost.util.config.Option;
 import dev.yurisuika.compost.util.config.options.Produce;
-import net.minecraft.world.item.ItemStack;
 
 import java.util.List;
 
@@ -46,19 +44,6 @@ public class CompostApi {
      */
     public static void removeProduce(String name, Produce produce) {
         Option.removeProduce(name, produce);
-    }
-
-    /**
-     * <p>Retrieves an itemstack list sent from the server to the client.
-     *
-     * <p>An itemstack list is generated from the configured produce for the loaded level. The count for each itemstack
-     * is randomly chosen from an inclusive range of the configured max and min of the produce. Upon a client joining
-     * the server or the config being saved, the server sends this list to the client.
-     *
-     * @return an {@link ItemStack} {@link List}
-     */
-    public static List<ItemStack> getStacks() {
-        return Network.getStacks();
     }
 
 }
