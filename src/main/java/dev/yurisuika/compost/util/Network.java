d4b8a686a8ac58a258621acd5f08b722edb9c88a
yurisuika <yurisuika@protonmail.com>
2025-04-30 08:24:22 -0700

    2.x update - reworked configuration and commands, fixes for pipe mods and NF hopper hook

--- src/main/java/dev/yurisuika/compost/util/Network.java
+++ src/main/java/dev/yurisuika/compost/util/Network.java
@@ -1,9 +1,10 @@
 package dev.yurisuika.compost.util;
 
-import dev.yurisuika.compost.network.protocol.common.ClientboundProducePacket;
+import dev.yurisuika.compost.network.protocol.common.ClientboundCompostPacket;
 import dev.yurisuika.compost.network.protocol.common.ClientboundResetPacket;
+import dev.yurisuika.compost.network.protocol.common.ClientboundWorldPacket;
 import dev.yurisuika.compost.util.config.Option;
-import dev.yurisuika.compost.util.config.options.Produce;
+import dev.yurisuika.compost.util.config.options.Composition;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.FriendlyByteBuf;
@@ -11,38 +12,49 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Network {
 
-    public static List<Produce> produce = new ArrayList<>();
+    public static String levelName;
+    public static Map<String, Composition> networkCompositions = new HashMap<>();
 
-    public static List<Produce> getProduce() {
-        return produce;
+    public static String getLevelName() {
+        return levelName;
     }
 
-    public static void setProduce(List<Produce> produce) {
-        Network.produce = produce;
+    public static void setLevelName(String levelName) {
+        Network.levelName = levelName;
     }
 
-    public static void sendProduce(Level level, Player player) {
+    public static Map<String, Composition> getNetworkCompositions() {
+        return networkCompositions;
+    }
+
+    public static void setNetworkCompositions(Map<String, Composition> networkCompositions) {
+        Network.networkCompositions = networkCompositions;
+    }
+
+    public static void sendCompositions(Level level, Player player) {
         if (!level.isClientSide()) {
             ServerPlayNetworking.send((ServerPlayer) player, ClientboundResetPacket.ID, PacketByteBufs.empty());
-            setProduce(new ArrayList<>());
-            Option.getWorlds().forEach(world -> {
-                if (Objects.equals(world.getName(), level.getServer().getWorldData().getLevelName())) {
-                    world.getProduce().forEach(produce -> {
-                        FriendlyByteBuf buffer = PacketByteBufs.create();
-                        buffer.writeUtf(produce.getItem());
-                        buffer.writeDouble(produce.getChance());
-                        buffer.writeInt(produce.getMin());
-                        buffer.writeInt(produce.getMax());
-                        ServerPlayNetworking.send((ServerPlayer) player, ClientboundProducePacket.ID, buffer);
-                        getProduce().add(produce);
-                    });
-                }
+            getNetworkCompositions().clear();
+            Option.getCompositions().forEach((name, composition) -> {
+                FriendlyByteBuf compostBuffer = PacketByteBufs.create();
+                compostBuffer.writeUtf(name);
+                compostBuffer.writeUtf(composition.getCompost().getItem());
+                compostBuffer.writeDouble(composition.getCompost().getChance());
+                compostBuffer.writeInt(composition.getCompost().getCount().getMin());
+                compostBuffer.writeInt(composition.getCompost().getCount().getMax());
+                ServerPlayNetworking.send((ServerPlayer) player, ClientboundCompostPacket.ID, compostBuffer);
+                composition.getWorlds().forEach(world -> {
+                    FriendlyByteBuf worldBuffer = PacketByteBufs.create();
+                    worldBuffer.writeUtf(name);
+                    worldBuffer.writeUtf(composition.getCompost().getItem());
+                    ServerPlayNetworking.send((ServerPlayer) player, ClientboundWorldPacket.ID, worldBuffer);
+                });
+                getNetworkCompositions().put(name, composition);
             });
         }
     }
