65e3071adb3c3a4bb578ba24c7a24e1ea369667b
yurisuika <yurisuika@protonmail.com>
2025-04-30 08:24:32 -0700

    2.x update - reworked configuration and commands, fixes for pipe mods and NF hopper hook

---
+++ src/main/java/dev/yurisuika/compost/network/protocol/common/ClientboundCompostPacket.java
@@ -0,0 +1,33 @@
+package dev.yurisuika.compost.network.protocol.common;
+
+import dev.yurisuika.compost.util.Network;
+import dev.yurisuika.compost.util.config.options.Composition;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.HashSet;
+
+public record ClientboundCompostPacket(String name, String item, Double chance, Integer min, Integer max) {
+
+    public static final ResourceLocation ID = ResourceLocation.tryParse("compost:compost");
+
+    public ClientboundCompostPacket(FriendlyByteBuf buffer) {
+        this(buffer.readUtf(), buffer.readUtf(), buffer.readDouble(), buffer.readInt(), buffer.readInt());
+    }
+
+    public void write(FriendlyByteBuf buffer) {
+        buffer.writeUtf(name());
+        buffer.writeUtf(item());
+        buffer.writeDouble(chance());
+        buffer.writeInt(min());
+        buffer.writeInt(max());
+    }
+
+    public static void handle(Minecraft minecraft, ClientPacketListener listener, FriendlyByteBuf buffer, PacketSender sender) {
+        Network.getNetworkCompositions().put(buffer.readUtf(), new Composition(new Composition.Compost(buffer.readUtf(), buffer.readDouble(), new Composition.Compost.Count(buffer.readInt(), buffer.readInt())), new HashSet<>()));
+    }
+
+}
--- src/main/java/dev/yurisuika/compost/util/Parse.java
+++ src/main/java/dev/yurisuika/compost/util/Parse.java
@@ -2,8 +2,8 @@ package dev.yurisuika.compost.util;
 
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import dev.yurisuika.compost.util.config.options.Produce;
-import net.minecraft.CharPredicate;
+import dev.yurisuika.compost.util.config.Option;
+import dev.yurisuika.compost.util.config.options.Composition;
 import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.arguments.item.ItemArgument;
 import net.minecraft.core.RegistryAccess;
@@ -11,32 +11,49 @@ import net.minecraft.world.item.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Parse {
 
-    public static List<String> listItems() {
-        List<String> items = new ArrayList<>();
-        for (Produce produce : Network.getProduce()) {
-            items.add(produce.getItem());
-        }
-        return items;
+    public static List<String> listNames(Map<String, Composition> compositions) {
+        List<String> names = new ArrayList<>();
+        compositions.forEach((name, composition) -> names.add(name));
+        return names;
     }
 
-    public static ItemStack createItemStack(Produce produce) {
+    public static List<String> listLocalNames() {
+        return listNames(Option.getCompositions());
+    }
+
+    public static List<String> listNetworkNames() {
+        return listNames(Network.getNetworkCompositions());
+    }
+
+    public static ItemStack createCompost(Composition composition) {
         try {
-            return new ItemArgument(new CommandBuildContext(RegistryAccess.BUILTIN.get())).parse(new StringReader(produce.getItem())).createItemStack(ThreadLocalRandom.current().nextInt(produce.getMin(), produce.getMax() + 1), true);
+            return new ItemArgument(new CommandBuildContext(RegistryAccess.BUILTIN.get())).parse(new StringReader(composition.getCompost().getItem())).createItemStack(ThreadLocalRandom.current().nextInt(composition.getCompost().getCount().getMin(), composition.getCompost().getCount().getMax() + 1), true);
         } catch (CommandSyntaxException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static String readWhile(StringReader reader, CharPredicate predicate) {
-        int i = reader.getCursor();
-        while(reader.canRead() && predicate.test(reader.peek())) {
-            reader.skip();
-        }
-        return reader.getString().substring(i, reader.getCursor());
+    public static List<ItemStack> createCompostOutput(Map<String, Composition> compositions, String level) {
+        List<ItemStack> compost = new ArrayList<>();
+        compositions.forEach((name, composition) -> {
+            if (composition.getWorlds().isEmpty() || composition.getWorlds().contains(level)) {
+                compost.add(createCompost(composition));
+            }
+        });
+        return compost;
+    }
+
+    public static List<ItemStack> createLocalCompostOutput(String level) {
+        return createCompostOutput(Option.getCompositions(), level);
+    }
+
+    public static List<ItemStack> createNetworkCompostOutput(String level) {
+        return createCompostOutput(Network.getNetworkCompositions(), level);
     }
 
 }
