b385ebdf7b9138961b51e0bc5978da890316b00e
yurisuika <yurisuika@protonmail.com>
2025-06-06 06:50:59 -0700

    readd override annotations

--- src/main/java/dev/yurisuika/compost/world/level/block/ContainerComposterBlock.java
+++ src/main/java/dev/yurisuika/compost/world/level/block/ContainerComposterBlock.java
@@ -35,6 +35,7 @@ public class ContainerComposterBlock extends ComposterBlock implements EntityBlo
         super(properties);
     }
 
+    @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new ContainerComposterBlockEntity(pos, state);
     }
@@ -55,9 +56,7 @@ public class ContainerComposterBlock extends ComposterBlock implements EntityBlo
             if (i < 7 && !level.isClientSide) {
                 level.levelEvent(LevelEvent.COMPOSTER_FILL, pos, state != ComposterBlockInvoker.invokeAddItem(player, state, level, pos, stack) ? 1 : 0);
                 player.awardStat(Stats.ITEM_USED.get(stack.getItem()));
-                if (!player.getAbilities().instabuild) {
-                    stack.shrink(1);
-                }
+                stack.consume(1, player);
             }
             return InteractionResult.SUCCESS;
         } else {
@@ -65,6 +64,7 @@ public class ContainerComposterBlock extends ComposterBlock implements EntityBlo
         }
     }
 
+    @Override
     public InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit) {
         if (state.getValue(LEVEL) == 8) {
             extractProduce(player, state, level, pos);
@@ -96,6 +96,7 @@ public class ContainerComposterBlock extends ComposterBlock implements EntityBlo
         return blockState;
     }
 
+    @Override
     public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
         if (state.getValue(LEVEL) == 7) {
             ContainerComposterBlockEntity blockEntity = (ContainerComposterBlockEntity) level.getBlockEntity(pos);
@@ -110,6 +111,7 @@ public class ContainerComposterBlock extends ComposterBlock implements EntityBlo
         }
     }
 
+    @Override
     public WorldlyContainer getContainer(BlockState state, LevelAccessor level, BlockPos pos) {
         return (WorldlyContainer) level.getBlockEntity(pos);
     }
