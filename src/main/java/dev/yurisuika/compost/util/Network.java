832bbd7bb54c343dd2659809caaa38cf4cab0c8c
yurisuika <yurisuika@protonmail.com>
2025-06-26 04:31:41 -0700

    write resourcelocation to buffer for 20.2+ payload packet

--- src/main/java/dev/yurisuika/compost/util/Network.java
+++ src/main/java/dev/yurisuika/compost/util/Network.java
@@ -6,7 +6,7 @@ import dev.yurisuika.compost.network.protocol.common.ClientboundWorldPacket;
 import dev.yurisuika.compost.world.Composition;
 import io.netty.buffer.Unpooled;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
+import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
 
@@ -29,21 +29,24 @@ public class Network {
     public static void sendCompositions(Level level, ServerPlayer player) {
         if (!level.isClientSide()) {
             FriendlyByteBuf resetBuffer = new FriendlyByteBuf(Unpooled.EMPTY_BUFFER);
-            player.connection.send(new ClientboundCustomPayloadPacket(ClientboundResetPacket.ID, resetBuffer));
+            resetBuffer.writeResourceLocation(ClientboundResetPacket.ID);
+            player.connection.send(new ClientboundCustomPayloadPacket(resetBuffer));
             COMPOSITIONS.clear();
             Configure.getCompositions().forEach((name, composition) -> {
                 FriendlyByteBuf compostBuffer = new FriendlyByteBuf(Unpooled.buffer());
+                compostBuffer.writeResourceLocation(ClientboundCompostPacket.ID);
                 compostBuffer.writeUtf(name);
                 compostBuffer.writeUtf(composition.getCompost().getItem());
                 compostBuffer.writeDouble(composition.getCompost().getChance());
                 compostBuffer.writeInt(composition.getCompost().getCount().getMin());
                 compostBuffer.writeInt(composition.getCompost().getCount().getMax());
-                player.connection.send(new ClientboundCustomPayloadPacket(ClientboundCompostPacket.ID, compostBuffer));
+                player.connection.send(new ClientboundCustomPayloadPacket(compostBuffer));
                 composition.getWorlds().forEach(world -> {
                     FriendlyByteBuf worldBuffer = new FriendlyByteBuf(Unpooled.buffer());
+                    worldBuffer.writeResourceLocation(ClientboundWorldPacket.ID);
                     worldBuffer.writeUtf(name);
                     worldBuffer.writeUtf(composition.getCompost().getItem());
-                    player.connection.send(new ClientboundCustomPayloadPacket(ClientboundWorldPacket.ID, worldBuffer));
+                    player.connection.send(new ClientboundCustomPayloadPacket(worldBuffer));
                 });
                 COMPOSITIONS.put(name, composition);
             });
