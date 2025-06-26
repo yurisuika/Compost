7b89975e97ee4b0c125eafb82d3ca1d90ab01bfb
yurisuika <yurisuika@protonmail.com>
2025-06-26 00:45:44 -0700

    Restructure a bit

--- src/main/java/dev/yurisuika/compost/network/protocol/common/custom/CompostPayload.java
+++ src/main/java/dev/yurisuika/compost/network/protocol/common/custom/CompostPayload.java
@@ -1,7 +1,7 @@
 package dev.yurisuika.compost.network.protocol.common.custom;
 
 import dev.yurisuika.compost.util.Network;
-import dev.yurisuika.compost.util.config.options.Composition;
+import dev.yurisuika.compost.world.Composition;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
