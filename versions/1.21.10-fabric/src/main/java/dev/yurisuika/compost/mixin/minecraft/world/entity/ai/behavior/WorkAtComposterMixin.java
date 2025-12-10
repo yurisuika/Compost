693b258d8a9b013a284100c4b37e0f13f28cbbfc
yurisuika <yurisuika@proton.me>
2025-12-09 22:43:23 -0800

    break out 1.21.10 support from 1.21.11 fabric

---
+++ versions/1.21.10-fabric/src/main/java/dev/yurisuika/compost/mixin/minecraft/world/entity/ai/behavior/WorkAtComposterMixin.java
@@ -0,0 +1,28 @@
+package dev.yurisuika.compost.mixin.minecraft.world.entity.ai.behavior;
+
+import dev.yurisuika.compost.world.level.block.ContainerComposterBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.ai.behavior.WorkAtComposter;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(WorkAtComposter.class)
+public class WorkAtComposterMixin {
+
+    @Redirect(method = "compostItems", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/ComposterBlock;insertItem(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"))
+    private BlockState redirectComposter(Entity entity, BlockState state, ServerLevel level, ItemStack stack, BlockPos pos) {
+        return ContainerComposterBlock.insertItem(entity, state, level, stack, pos);
+    }
+
+    @Redirect(method = "compostItems", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/ComposterBlock;extractProduce(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"))
+    private BlockState redirectComposter(Entity entity, BlockState state, Level level, BlockPos pos) {
+        return ContainerComposterBlock.extractProduce(entity, state, level, pos);
+    }
+
+}
